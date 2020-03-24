49d99633837f1f8ebb230120542a80d973e98cc4
Bart van Meerkerk <bart@gb-development.nl>
2020-03-24 12:26:56 +0100

    Add project files.

---
+++ OverDeRheinKraanKeuringen/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OverDeRheinKraanKeuringen.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
