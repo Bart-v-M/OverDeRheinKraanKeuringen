80c800a0ecf20997be861fc77713c0740ebf92ed
Bart van Meerkerk <bart@gb-development.nl>
2020-03-24 19:47:25 +0100

    Bug fix in HomeController: now the assigments in the database are requested and received.

--- OverDeRheinKraanKeuringen/Controllers/HomeController.cs
+++ OverDeRheinKraanKeuringen/Controllers/HomeController.cs
@@ -4,15 +4,25 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OverDeRheinKraanKeuringen.Data;
 using OverDeRheinKraanKeuringen.Models;
 
 namespace OverDeRheinKraanKeuringen.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: Assignments
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Assignments.ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
